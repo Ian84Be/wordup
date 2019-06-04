fa958d9e8356d9206ecb100f0f634989de7da4ca
Ian Be <ian84be@gmail.com>
2019-06-03 20:02:02 -0500

    create react-app

---
+++ wordgame/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './wu.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="logo" alt="logo" src={logo} />
+    </div>
+  );
+}
+
+export default App;
