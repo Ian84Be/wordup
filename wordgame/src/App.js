dc44727ccf2ae83bdbaf16d7993427b7daf262fb
Ian Be <ian84be@gmail.com>
2019-06-11 14:37:23 -0500

    reorganize && tweak App functions, strictModeScoring works perfectly (FINALLY), dictionaryCheck is ENABLED, ready for limited release

--- wordgame/src/App.js
+++ wordgame/src/App.js
@@ -6,8 +6,8 @@ import ScoreBoard from './Components/ScoreBoard/ScoreBoard.js'
 
 import './App.scss';
 
-// dictionary DISABLED for testing
-// import scrabbleWordList from './scrabbleWordList.js';
+// dictionary DISABLE for testing
+import scrabbleWordList from './scrabbleWordList.js';
 
 export default class App extends React.Component {
   state = { 
@@ -67,7 +67,6 @@ export default class App extends React.Component {
     const newBoard = [...this.state.gameBoard];
     const thisTile = newBoard[index];
     const thisLetter = thisTile.stack.shift();
-    // if (!thisLetter) return console.log('NO LETTERZ');
     thisTile.active = false;
     this.setState(prevState => ({
       ...prevState,
@@ -77,183 +76,13 @@ export default class App extends React.Component {
     }));
   }
 
-  nextPlayer = (addScore) => {
-    const oldLetters = [...this.state.myLetters];
-    let newLetters=[],randomLetters=[];
-    if (addScore === 0) newLetters = drawLetters(8);
-    if (this.state.myLetters.length<8) {
-      randomLetters = drawLetters(8-this.state.myLetters.length);
-      newLetters = [...oldLetters,...randomLetters];
-    }
-    // TODO
-    // powerup E9 tile if theirScore - myScore > 40
-    // toggle config obj >> guarantee ONE VOWEL
-    // fix so that hand INCLUDES one vowel >> put this on App method for nextPlayer
-    const vowels = ['A','E','I','O','U'];
-    let hasVowel = 0;
-    vowels.forEach(vowel => {
-      if (newLetters.includes(vowel)) ++hasVowel;
-    });
-    if (!hasVowel) {
-      let randomV = vowels[Math.floor(Math.random()*5)];
-      console.log('randomV',randomV);
-      newLetters.pop();
-      newLetters.push(randomV);
-    }
-    this.setState(prevState => ({
-      ...prevState,
-      activeTiles:[],
-      message: '',
-      myLetters: newLetters,
-      myScore: prevState.myScore + addScore
-    }))
-  }
-
-  onDragStart = (e, index, isActive = false) => {
-    // console.log('dragSTART index',index);
-    if (typeof(index) !== 'number') return e.dataTransfer.setData("letter", index);
-    else if (!isActive) return;
-    else {
-      const thisTile = this.state.gameBoard[index];
-      e.dataTransfer.setData("incomingIndex", index);
-      e.dataTransfer.setData("letter", thisTile.stack[0]);
-    }
-  }
-
-  onDrop = (e, droppedOnIndex, isActive) => {
-    if (isActive) return this.setState(() => ({message: `cannot replace active tile!`}));
-    const letter = e.dataTransfer.getData("letter");
-    if (letter === '') return;
-    const newBoard = [...this.state.gameBoard];
-    const myNewLetters = [...this.state.myLetters];
-    const droppedOnTile = newBoard[droppedOnIndex];
-    if (droppedOnTile.stack[0] === letter) return this.setState(() => ({message: `this letter is already ${letter}!`}));
-    const incomingIndex = e.dataTransfer.getData("incomingIndex");
-    if (incomingIndex) {
-      const incomingTile = newBoard[incomingIndex];
-      incomingTile.stack.shift();
-      droppedOnTile.stack.unshift(letter);
-      incomingTile.active = false;
-    } else {
-      myNewLetters.splice(myNewLetters.indexOf(letter),1)
-      droppedOnTile.stack.unshift(letter);
-    }
-    droppedOnTile.active = true;
-    this.setState(prevState => ({
-      ...prevState,
-      gameBoard: newBoard,
-      message: '',
-      myLetters: myNewLetters
-    }));
-  }
-
-  passTurn = () => {
-    const activeTiles = this.state.gameBoard.filter(tile => tile.active);
-    if (activeTiles.length>0) return this.setState(() => ({message: 'cannot pass with active tiles on board!'}));
-    this.nextPlayer(0);
-  } // this.passTurn() END >> this.nextPlayer(0);
-
-  submitLetters = () => {
-    const activeTiles = this.state.gameBoard.filter(tile => tile.active).sort((a,b) => a.id-b.id);
-    if (activeTiles.length<1) return this.setState(() => ({message: 'you havent placed any tiles!'}));
-    else this.findWords(activeTiles);
-  } // this.submitLetters END >> this.findWords(activeTiles);
-
-  // this.scoreWords() START >> strictModeScoring(foundWords) >> this.calculateScore(foundWords)
-  scoreWords = (foundWords) => {
-    // TODO
-    // double strict scoring >> lose turn if dictionary FAIL
-    const newBoard = [...this.state.gameBoard];
-    let okStrict = strictModeScoring(foundWords);
-    if (!okStrict) return console.log('error: strictModeScoring VIOLATION');
-    const activeTiles = newBoard.filter(tile => tile.active);
-
-    let scoreInfo = this.calculateScore(foundWords);
-    // console.log('scoreWords scoreInfo',scoreInfo);
-    const score = scoreInfo[0];
-    const words = scoreInfo[1];
-    console.log('score',score,words);
-    
-    if (score !== 'fail') {
-      // console.log('scoreWords PASS activeTiles',activeTiles);
-      activeTiles.forEach((tile) => {
-        let thisIndex = newBoard.findIndex(that => that.id === tile.id);
-        newBoard[thisIndex].active = false;
-      });
-      this.setState(prevState => ({
-        ...prevState,
-        gameBoard: newBoard,
-        message: `you scored ${score} !`,
-        myHistory: [...prevState.myHistory, words]
-      }));
-      return this.nextPlayer(score);
-    } else {
-      console.log('scoreWords FAIL');
-      return this.setState(() => ({message: `dictionary FAIL ${words}`}));
-    }
-
-    // strict scoring mode >> force players to build in ONE DIRECTION ONLY
-    function strictModeScoring(foundWords) {
-      console.log('strictModeScoring(foundWords)',foundWords);
-
-      let vertActive = [];
-      let horActive = [];
-      foundWords.forEach(tileSet => {
-        let direction = tileSet[0];
-        let activeTiles = tileSet[2];
-        (direction === 'vert') ? vertActive.push(...activeTiles) : horActive.push(...activeTiles);
-      });
-      console.log('vertActive',vertActive,'horActive',horActive);
-      const activeTiles = newBoard.filter(tile => tile.active);
-      console.log('activeTiles',activeTiles);
-      // if (activeIds.length>0) return console.log('error: loose tiles');
-
-      if (vertActive.length === 1 && horActive.length === 1) {
-        if (vertActive[0] !== horActive[0]) return console.log('strictMode FAIL >> cannot build in two directions!');
-      }
-
-      let oneColumn = true;
-      if (vertActive.length>0) oneColumn = vertActive.every(id => id % 10 === vertActive[0] % 10);
-
-      let oneRow = true;
-      let leftLimit = horActive[0] - (horActive[0] % 10);
-      let rightLimit = leftLimit + 7;
-      horActive.forEach((id, i) => {
-        if (id < leftLimit || id > rightLimit) oneRow = false;
-      });
-      console.log('oneRow',oneRow);
-      console.log('oneColumn',oneColumn);
-
-      if (!oneColumn || !oneRow) return console.log('strictMode FAIL >> cannot build in two directions!');
-      if (oneColumn && oneRow) return console.log('strictMode FAIL >> cannot build in two directions!');
-      // let oneRow = horActive.every(id => {})
-      return true;
-
-      // if (vertActive.length === 1 && horActive.length === 1) {
-      //   if (vertActive[0] !== horActive[0]) return console.log('strictMode FAIL >> cannot build in two directions!');
-      // } else if (vertActive.length>1 && horActive.length>1) {
-      //   let isEqual = true;
-      //   vertActive.forEach(tile => {
-      //     if (!horActive.includes(tile)) isEqual = false;
-      //   });
-      //   if (!isEqual) return console.log('strictMode FAIL >> cannot build in two directions!');
-      // }
-      
-    } // strictModeScoring() END 
-  } // this.scoreWords() END >> return this.nextPlayer(score);
-  
   calculateScore = (foundWords) => {
     // check the tileSet of each foundWord, push the letter to tempWord, score the letter based on stack length
     let words=[],thisWord='',score=0;
       foundWords.forEach(tileSet => {
         let tempScore = 0;
         let tempWord = [];
-        // console.log('calculateScore tileSet[0]',tileSet[0]);
-        // console.log('calculateScore tileSet[1]',tileSet[1]);
           tileSet[1].forEach(tile => {
-            // console.log('scoring tile',tile);
-            // console.log('tile.active',tile.active);
-            // if (tile.active) tile.active = false;
             tempWord.push(tile.stack[0]);
             return tempScore = tempScore + tile.stack.length;
           });
@@ -274,8 +103,8 @@ export default class App extends React.Component {
 
   dictionaryCheck = (word) => {
     // return false;
-    return true;
-    // return (scrabbleWordList.includes(word)) ? true : false;
+    // return true;
+    return (scrabbleWordList.includes(word)) ? true : false;
   } // this.dictionaryCheck() END >> back to this.calculateScore();
 
   // this.findWords() START 
@@ -285,9 +114,8 @@ export default class App extends React.Component {
   findWords = (activeTiles) => {
     const tempWords = [];
     activeTiles.forEach(tile => tempWords.push(...this.lookBothWays(tile)));
-    // console.log('!!! ### !!! findWords tempWords',tempWords);
     let foundWords = this.uniqWords(tempWords);
-    console.log(foundWords.length,'uniqWords',foundWords);
+    // console.log(foundWords.length,'uniqWords',foundWords);
     if (foundWords.length>0) return this.scoreWords(foundWords);
   } // this.findWords() END >> return this.scoreWords(foundWords);
 
@@ -352,6 +180,190 @@ export default class App extends React.Component {
       } else return null;
     } // startLook() END
   } // lookBothWays() END >> back to this.findWords()
+
+  nextPlayer = (addScore) => {
+    const oldLetters = [...this.state.myLetters];
+    let newLetters=[],randomLetters=[];
+    if (addScore === 0) newLetters = drawLetters(8);
+    if (this.state.myLetters.length<8) {
+      randomLetters = drawLetters(8-this.state.myLetters.length);
+      newLetters = [...oldLetters,...randomLetters];
+    }
+    // TODO
+    // powerup E9 tile if theirScore - myScore > 40
+    // toggle config obj >> guarantee ONE VOWEL
+    // fix so that hand INCLUDES one vowel >> put this on App method for nextPlayer
+    const vowels = ['A','E','I','O','U'];
+    let hasVowel = 0;
+    vowels.forEach(vowel => {
+      if (newLetters.includes(vowel)) ++hasVowel;
+    });
+    if (!hasVowel) {
+      let randomV = vowels[Math.floor(Math.random()*5)];
+      console.log('randomV',randomV);
+      newLetters.pop();
+      newLetters.push(randomV);
+    }
+    this.setState(prevState => ({
+      ...prevState,
+      activeTiles:[],
+      message: '',
+      myLetters: newLetters,
+      myScore: prevState.myScore + addScore
+    }))
+  }
+
+  onDragStart = (e, index, isActive = false) => {
+    if (typeof(index) !== 'number') return e.dataTransfer.setData("letter", index);
+    else if (!isActive) return;
+    else {
+      const thisTile = this.state.gameBoard[index];
+      e.dataTransfer.setData("incomingIndex", index);
+      e.dataTransfer.setData("letter", thisTile.stack[0]);
+    }
+  }
+
+  onDrop = (e, droppedOnIndex, isActive) => {
+    if (isActive) return this.setState(() => ({message: `cannot replace active tile!`}));
+    const letter = e.dataTransfer.getData("letter");
+    if (letter === '') return;
+    const newBoard = [...this.state.gameBoard];
+    const myNewLetters = [...this.state.myLetters];
+    const droppedOnTile = newBoard[droppedOnIndex];
+    if (droppedOnTile.stack[0] === letter) return this.setState(() => ({message: `this letter is already ${letter}!`}));
+    const incomingIndex = e.dataTransfer.getData("incomingIndex");
+    if (incomingIndex) {
+      const incomingTile = newBoard[incomingIndex];
+      incomingTile.stack.shift();
+      droppedOnTile.stack.unshift(letter);
+      incomingTile.active = false;
+    } else {
+      myNewLetters.splice(myNewLetters.indexOf(letter),1)
+      droppedOnTile.stack.unshift(letter);
+    }
+    droppedOnTile.active = true;
+    this.setState(prevState => ({
+      ...prevState,
+      gameBoard: newBoard,
+      message: '',
+      myLetters: myNewLetters
+    }));
+  }
+
+  passTurn = () => {
+    const activeTiles = this.state.gameBoard.filter(tile => tile.active);
+    if (activeTiles.length>0) return this.setState(() => ({message: 'cannot pass with active tiles on board!'}));
+    this.nextPlayer(0);
+  } // this.passTurn() END >> this.nextPlayer(0);
+
+  submitLetters = () => {
+    const activeTiles = this.state.gameBoard.filter(tile => tile.active).sort((a,b) => a.id-b.id);
+    if (activeTiles.length<1) return this.setState(() => ({message: 'you havent placed any tiles!'}));
+    else this.findWords(activeTiles);
+  } // this.submitLetters END >> this.findWords(activeTiles);
+
+  // this.scoreWords() START >> strictModeScoring(foundWords) >> this.calculateScore(foundWords)
+  scoreWords = (foundWords) => {
+    const newBoard = [...this.state.gameBoard];
+    let okStrict = strictModeScoring(foundWords);
+    if (!okStrict) {
+      console.log('error: strictModeScoring VIOLATION');
+      return this.setState(() => ({message: 'error: cannot build in both directions!'}));
+    } 
+    const activeTiles = newBoard.filter(tile => tile.active);
+
+    let scoreInfo = this.calculateScore(foundWords);
+    const score = scoreInfo[0];
+    const words = scoreInfo[1];
+    console.log('score',score,words);
+    
+    if (typeof(score) === 'number') {
+      activeTiles.forEach((tile) => {
+        let thisIndex = newBoard.findIndex(that => that.id === tile.id);
+        newBoard[thisIndex].active = false;
+      });
+      this.setState(prevState => ({
+        ...prevState,
+        gameBoard: newBoard,
+        message: `you scored ${score} !`,
+        myHistory: [...prevState.myHistory, words]
+      }));
+      return this.nextPlayer(score);
+    } else return this.setState(() => ({message: `dictionary FAIL ${words}`}));
+    // TODO
+    // double strict scoring >> lose turn if dictionary FAIL
+
+    // strictModeScoring() START 
+    // >> lineLook()
+    function strictModeScoring(foundWords) {
+      // console.log('strictModeScoring',foundWords);
+      let vertActive = [];
+      let horActive = [];
+      foundWords.forEach(tileSet => {
+        let direction = tileSet[0];
+        let activeTiles = tileSet[2];
+        (direction === 'vert') ? vertActive.push(...activeTiles) : horActive.push(...activeTiles);
+      });
+        // console.log('vertActive',vertActive,'horActive',horActive);
+      
+      const activeTiles = newBoard.filter(tile => tile.active);
+      let vertDupe = activeTiles.every(tile => vertActive.includes(tile.id));
+      let horDupe = activeTiles.every(tile => horActive.includes(tile.id));
+      let dupeCheck = (vertDupe && horDupe) ? true : false;
+        // console.log('vertDupe',vertDupe,'horDupe',horDupe,'dupeCheck',dupeCheck);
+      if (vertDupe && horActive.length<2) dupeCheck = true;
+      if (horDupe && vertActive.length<2) dupeCheck = true;
+        // console.log('dupeCheck',dupeCheck);
+
+      let uniqActive = new Set();
+      for (let ids of vertActive) uniqActive.add(ids);
+      for (let ids of horActive) uniqActive.add(ids);
+        // console.log('activeTiles',activeTiles);
+        // console.log('uniqActive',uniqActive);
+      if (uniqActive.size < activeTiles.length) return console.log('error: loose tiles');
+      let okHor, okVert;
+
+      if (vertActive.length > 0) okVert = lineLook('vert',vertActive);
+      else okVert=0;
+      // console.log('okVert',okVert);
+      
+      if (horActive.length > 0) okHor = lineLook('hor',horActive);
+      else okHor=0;
+        // console.log('okHor',okHor);
+
+        // console.log('okVert',okVert,'okHor',okHor);
+        // console.log('dupeCheck',dupeCheck);
+      if (okVert && okHor === 0) return (dupeCheck) ? true : false;
+      if (okHor && okVert === 0) return true;
+      if (okVert && vertDupe) return true;
+      if (okHor && horDupe) return true;
+      if (okVert === 0 && okHor === 0) return (dupeCheck) ? true : false;
+      else return false;
+
+      function lineLook(direction, line) {
+        let okVert,okHor,result=false;
+        let startTile = line[0];
+        // console.log('startTile',startTile);
+        if (direction === 'hor') {
+          // let thisRow = `${startTile}`.charAt(0);
+          // console.log('thisRow',thisRow);
+          let leftLimit = startTile - (startTile % 10);
+          let rightLimit = leftLimit + 7;
+          // console.log('leftLimit',leftLimit,'/',startTile,'/',rightLimit,' rightLimit');
+          okHor = line.every(id => (id >= leftLimit && id <= rightLimit));
+          if (okHor) result = true;
+        }
+        if (direction === 'vert') {
+          // let thisCol = `${startTile}`.charAt(1);
+          // console.log('thisCol',thisCol);
+          okVert = line.every(id => (id % 10 === startTile % 10));
+          // console.log('lineLook() okVert',okVert);
+          if (okVert) result = true;
+        }
+        return result;
+      } // lineLook() END >> back to strictModeScoring()
+    } // strictModeScoring() END 
+  } // this.scoreWords() END >> return this.nextPlayer(score);
 
   uniqWords = (tempWords) => {
     const seen = {};
