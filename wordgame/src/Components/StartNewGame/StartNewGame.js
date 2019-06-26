03264634617a2e82d0bc3a383162cdff28544a98
Ian Be <ian84be@gmail.com>
2019-06-26 13:59:35 -0500

    adjust responsive design, restyle instructions

--- wordgame/src/Components/StartNewGame/StartNewGame.js
+++ wordgame/src/Components/StartNewGame/StartNewGame.js
@@ -83,23 +83,37 @@ const StartNewGame = (props) => {
             
                     <h2>HOW - TO - PLAY</h2>
             <div className="rules">
-                    <p>Place tiles on the board to build words!</p>
-                    <p>The first player must cover <em>one or more</em> of the four <em>central squares</em>.</p>
-                    <p>All tiles played on a turn must form part of one continuous line of tiles <em>vertical </em> or <em>horizontal</em>.</p>
+                <ul>
+                    <li><p>Place tiles on the board to build words!</p></li>
+                    <li><p>The first player must cover <em>one or more</em> of the four <em>central squares</em>.</p></li>
+                    <li><p>All tiles played on a turn must form part of one continuous line of tiles <em>vertical </em> or <em>horizontal</em>.</p></li>
+                
+                    
                     <h3>Scoring</h3>
-                    <p>Words built with stacked letters score <em>1 point</em> per tile in the stack.</p>
-                    <p>Words built with <em>no stacked letters</em> score <em>2 points</em> per tile.</p>
-                    <p>2 <em>bonus points</em> for using "Qu" in a word with <em>no stacked letters</em>.</p>
-                    <p>20 <em>bonus points</em> for using all seven tiles in one turn.</p>
+                    <li><p>Words built with stacked letters score <br/><em>1 point</em> per tile in the stack.</p></li>
+                    <li><p>Words built with <em>no stacked letters</em> score <em>2 points</em> per tile.</p></li>
+                    <li><p>2 <em>bonus points</em> for using "Qu" in a word with <em>no stacked letters</em>.</p></li>
+                    <li><p>20 <em>bonus points</em> for using all seven tiles in one turn.</p></li>
+                    
+                    
+                    
+                    
                     <h3>End Game</h3>
-                    <p>The <em>game ends </em> when letterBag is empty and one player has used all of his/her tiles, or
-                    when letterBag is empty and every player passes in a single round.</p>
-                    <p>Players lose <em>5 points</em> for each unused tile they hold at the end of the game.</p>
+                    <li><p>The <em>game ends </em> when letterBag is empty and one player has used all of their tiles, or when letterBag is empty and every player passes in a single round.</p></li>
+                    <li><p>Players lose <em>5 points</em> for each unused tile they hold at the end of the game.</p></li>
+                    
+                    
+                    
                     <h3>Player Controls</h3>
-                    <p>PASS >> Pass your turn and draw seven new letters</p>
-                    <p>SHUFFLE >> Shuffle the display of your letters</p>
-                    <p>SUBMIT >> Submit played words for scoring</p>
-                    <p>CLEAR BOARD >> Clear all activeTiles from the GameBoard</p>
+                    <li><p>PASS >> Pass your turn and draw seven new letters</p></li>
+                    <li><p>SHUFFLE >> Shuffle the display of your letters</p></li>
+                    <li><p>SUBMIT >> Submit played words for scoring</p></li>
+                    <li><p>CLEAR BOARD >> Clear all activeTiles from the GameBoard</p></li>
+                    
+                   
+                    
+                    
+                </ul>
             </div>
 
 
