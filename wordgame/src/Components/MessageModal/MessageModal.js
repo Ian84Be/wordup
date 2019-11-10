3622da128e612267bd4209f40a0b5fea38637308
Ian Be <ian84be@gmail.com>
2019-11-09 19:46:22 -0600

    improve message modal

--- wordgame/src/Components/MessageModal/MessageModal.js
+++ wordgame/src/Components/MessageModal/MessageModal.js
@@ -1,14 +1,21 @@
-    
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { newMessage, newErrMsg } from '../../redux/commo/commoActions';
 
-
-const MessageModal = (props) => {
-    // let warning = String.fromCharCode(9888);
-    return ( 
-        <div className="MessageModal">
-            {props.message}
-        </div>
-    );
-}
+const MessageModal = props => {
+  const dispatch = useDispatch();
+  return (
+    <div className="MessageModal" onClick={clearMsg}>
+      <div className="MessageModal--body">
+        <p>{props.message}</p>
+        <button onClick={clearMsg}>OK</button>
+      </div>
+    </div>
+  );
+  function clearMsg() {
+    dispatch(newMessage(''));
+    dispatch(newErrMsg(''));
+  }
+};
 
 export default MessageModal;
